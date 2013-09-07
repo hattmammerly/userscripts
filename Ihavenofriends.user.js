73a4c363d42ec329b05895a89474f8def3d4a606
Matt <hattmammerly@gmail.com>
2013-09-06 22:33:04 -0400

    removed broken/nonuseful page and group stuff, updated for whichever layout I am still on

--- Ihavenofriends.user.js
+++ Ihavenofriends.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name        I hate my friends
 // @description for those of us who hate our friends
-// @namespace http://github.com/sirspazzolot/userscripts/
+// @namespace http://github.com/hattmammerly/userscripts/
 // @include *.facebook.com/*
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.js
 // @version 1.0
 // ==/UserScript==
-// based on http://userscripts.org/scripts/show/150411
+// Heavily based on http://userscripts.org/scripts/show/150411
+// But updated and I don't advertise or secretly like my website's page.
 
 function sleep(rem){ //rem for removed entries
     setInterval(function() {
@@ -20,22 +21,12 @@ function popup(rem) {
 
 var page = "";
 //button placement:
-if ($("#pagelet_friends").length > 0) {
-    $('.uiToolbarContent .rfloat').prepend("<div id=\"delete_buttons\" style=\"float:right;margin-left:5px;\"></label><label for=\"Ihavenofriends\" class=\"_11b uiButton uiToolbarButton\"><input type=\"submit\" id=\"mass_deleter\" value=\"Delete Selected Friends\"></label></div>");
+if ($("#pagelet_timeline_medley_friends").length > 0) {
+    $('#medley_header_friends').append("<div id=\"delete_buttons\" style=\"float:right;margin-left:5px;\"></label><label for=\"Ihavenofriends\" class=\"_11b uiButton uiToolbarButton\"><input type=\"submit\" id=\"mass_deleter\" value=\"Delete Selected Friends\"></label></div>");
     $('.stickyHeaderWrap .back').css('height', '60px');
     $('.fbTimelineSection.mtm').css('margin-top', '10px');
     page = "friends";
-} else if ($("#pagelet_like_stream").length > 0) {
-    $('.fbTimelineSection.mtm.timelineLikeStream').find('.clearfix.uiHeaderTop').prepend("<div id=\"delete_buttons\" style=\"float:right;margin-left:5px;\"></label><label for=\"Ihavenofriends\" class=\"profileEditButton uiButton uiButtonOverlay\"><input type=\"submit\" id=\"mass_deleter\" value=\"Delete Selected Pages\"></label></div>");
-    $('.stickyHeaderWrap .back').css('height', '60px');
-    $('.fbTimelineSection.mtm').css('margin-top', '10px');
-    page = "likes";
-}/* else if ($(location).attr("href") == "https://www.facebook.com/bookmarks/groups" || $(location).attr("href") === "http://www.facebook.com/bookmarks/groups") {
-    $(".clearfix .uiHeaderTop").prepend("<div id=\"delete_buttons\" style=\"float:right;margin-left:5px;\"></label><label for=\"Ihavenofriends\" class=\"uiHeaderActions uiButton\"><input type=\"submit\" id=\"mass_deleter\" value=\"Leave Selected Groups\"></label></div>");
-    $('.stickyHeaderWrap .back').css('height', '60px');
-    $('.fbTimelineSection.mtm').css('margin-top', '10px');
-    page = "groups";
-}//*/
+} 
 
 set_timer();
 
@@ -46,19 +37,12 @@ $("#mass_deleter").live("click",function() { //well .live() is deprecated in 1.7
         var profileid = $(this).attr('id');
         var a = document.createElement('script');
         if (page === "friends") {
-            a.innerHTML = "new AsyncRequest().setURI('/ajax/profile/removefriend.php').setData({ uid: " + profileid + ",norefresh:true }).send();";
-        } else if (page === "likes") {
-            a.innerHTML = "new AsyncRequest().setURI('/ajax/pages/fan_status.php').setData({ fbpage_id: " + profileid + ",add:false }).send();"; //figure out what parameters this script needs and how to get them
-        }/* else if (page === "groups") {
-            a.innerHTML = "new AsyncRequest().setURI('/ajax/bookmark/groups/leave/').setData({ group_id: " + profileid + ", norefresh:true, autocomplete:true }).send();";
-            //a.innerHTML = "new AsyncRequest().setURI('/ajax/groups/membership/leave.php').setData({ group_id: " + profileid + ",norefresh:true }).send();"; 
-        }//*/
+            a.innerHTML = "new AsyncRequest().setURI('/ajax/profile/removefriendconfirm.php').setData({ uid: " + profileid + ",norefresh:true }).send();";
+        }
         document.body.appendChild(a);
     });
 
-    /*if (page === "groups") {
-        setTimeout(function(){confirm_dialog()}, 1000);
-    }//*/
+    alert('It probably worked.');
 
     if (i === 0) {
         alert('Select at least one entry to remove.\nContact me if it\'s broken');
@@ -76,11 +60,7 @@ function set_timer() {
 function set_checkboxes() {
     if (page === "friends") {
         friends_checkboxes();
-    } else if (page === "likes") {
-         likes_checkboxes();
-    } else if (page === "groups") {
-        groups_checkboxes();
-    }
+    } 
 }
 
 function friends_checkboxes() {
@@ -120,45 +100,3 @@ function friends_checkboxes() {
         });
     }
 }
-
-function likes_checkboxes() {
-    $('li.mbs.uiFavoritesStory').each(function(index) {
-        var id = $(this).find('div.nameText').children('a').attr('data-hovercard');
-        if (!id) {
-            id = $(this).find('td.vTop.prm.fbStreamTimelineFavInfoContainer').attr('data-hovercard');
-        }
-        if (!id) {
-            id = '1';
-        }
-        var profileid = parseInt(/(\d+)/.exec(id)[1], 10);
-        if (!$(this).find('div.nameText').children().hasClass('marked')) {
-            $(this).find('div.nameText').prepend('<input type="checkbox" class="marked" id="' + profileid + '">');
-        }
-    });
-}//*/
-/*
-function groups_checkboxes(i) {
-    $('#pagelet_bookmark_seeall').find('li.seeAllItem.clearfix').each(function() {
-        var id = $(this).children('a[id^="privacy_icon_group"]').attr('data-hovercard');
-        if (!id) {
-            id = $(this).find('li.uiMenuItem[data-label="Leave Group"]').children('a.itemAnchor').attr("href");
-        }
-        if (!id) {
-            id = 1;
-        }
-        var profileid = parseInt(/(\d+)/.exec(id)[1], 10);
-        if (!$(this).children().hasClass('marked')) {
-            $(this).find('a.pvm.phs.itemLinkWithPrivacyIcon.hasTimestamp').css('width', '420px');
-            $(this).prepend('<input type="checkbox" class="marked" id="' + profileid + '">');
-        }
-    });
-}//*/
-/*
-function confirm_dialog() {
-    $(document).find('div._10.confirm_dialog.uiLayer._3qw').each(function() {
-       $(this).find('input[name="prevent_readd"]').prop('checked', 0); //don't prevent others from re-adding
-       $(this).find('input[name="confirmed"]').live("click", function(e) {
-            e.preventDefault();
-       });
-    });
-}//*/
